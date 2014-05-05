9731391ee64d1c34d481bc59ed5c8e9fb85b9fb9
longbai <slongbai@gmail.com>
2014-05-05 18:04:01 +0800

    add saveas key

--- src/qiniupfop/main.go
+++ src/qiniupfop/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/base64"
 	"flag"
 	"log"
 	"net/url"
@@ -24,6 +25,7 @@ func main() {
 	secretKey := flag.String("sk", "", "secret key")
 	pfop := flag.String("pfop", "", "pfop option")
 	notify := flag.String("n", "", "notify url")
+	saveas := flag.String("o", "", "output key, outputBucket:key")
 	flag.Parse()
 	if *bucket == "" || *key == "" || *accessKey == "" || *accessKey == "" || *pfop == "" {
 		flag.PrintDefaults()
@@ -32,12 +34,16 @@ func main() {
 	}
 	conf.ACCESS_KEY = *accessKey
 	conf.SECRET_KEY = *secretKey
+	fops := *pfop
+	if *saveas != "" {
+		fops = fops + "|saveas/" + base64.URLEncoding.EncodeToString([]byte(*saveas))
+	}
 
 	client := rs.New(nil)
 	param := url.Values{}
 	param.Set("bucket", *bucket)
 	param.Set("key", *key)
-	param.Set("fops", *pfop)
+	param.Set("fops", fops)
 	param.Set("notifyURL", *notify)
 	var ret pfopRet
 	err := client.Conn.CallWithForm(nil, &ret, "http://api.qiniu.com/pfop/", param)
