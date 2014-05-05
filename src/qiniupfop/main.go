0c55e5f11bbce8d7432c527c523fa75157976af2
longbai <slongbai@gmail.com>
2014-05-05 19:27:17 +0800

    add put

--- src/qiniupfop/main.go
+++ src/qiniupfop/main.go
@@ -5,10 +5,12 @@ import (
 	"flag"
 	"log"
 	"net/url"
+	"os"
 )
 
 import (
 	"github.com/qiniu/api/conf"
+	"github.com/qiniu/api/resumable/io"
 	"github.com/qiniu/api/rs"
 )
 
@@ -18,37 +20,93 @@ type pfopRet struct {
 	Error        string `json:"error"`
 }
 
+func pfop(pfops, bucket, key, notifyUrl string) (ret pfopRet, err error) {
+	client := rs.New(nil)
+	param := url.Values{}
+	param.Set("bucket", bucket)
+	param.Set("key", key)
+	param.Set("fops", pfops)
+	param.Set("notifyURL", notifyUrl)
+	err = client.Conn.CallWithForm(nil, &ret, "http://api.qiniu.com/pfop/", param)
+	return
+}
+
+func buildOps(convert, saveas string) string {
+	pfops := convert
+	if saveas != "" {
+		pfops = pfops + "|saveas/" + base64.URLEncoding.EncodeToString([]byte(saveas))
+	}
+	return pfops
+}
+
+func genToken(bucket, fops, notifyUrl string) string {
+	policy := rs.PutPolicy{
+		Scope:               bucket,
+		PersistentNotifyUrl: notifyUrl,
+		PersistentOps:       fops,
+	}
+	return policy.Token(nil)
+}
+
+func put(token, key, file string) (ret io.PutRet, err error) {
+	f, err := os.Open(file)
+	if err != nil {
+		log.Fatalln("file not exist")
+		return
+	}
+	stat, err := f.Stat()
+	if err != nil || stat.IsDir() {
+		log.Fatalln("invalid file")
+		return
+	}
+
+	blockNotify := func(blkIdx int, blkSize int, ret *io.BlkputRet) {
+		log.Println("size", stat.Size(), "block id", blkIdx, "offset", ret.Offset)
+	}
+
+	params := map[string]string{}
+	extra := &io.PutExtra{
+		ChunkSize: 8192,
+		Notify:    blockNotify,
+		Params:    params,
+	}
+
+	err = io.PutFile(nil, &ret, token, key, file, extra)
+	return
+}
+
 func main() {
+	file := flag.String("f", "", "local file")
 	bucket := flag.String("b", "", "bucket")
 	key := flag.String("k", "", "file key")
 	accessKey := flag.String("ak", "", "access key")
 	secretKey := flag.String("sk", "", "secret key")
-	pfop := flag.String("pfop", "", "pfop option")
+	convert := flag.String("c", "", "pfop single convert option avthumb/m3u8/segtime/10/vcodec/libx264/s/320x240")
 	notify := flag.String("n", "", "notify url")
 	saveas := flag.String("o", "", "output key, outputBucket:key")
 	flag.Parse()
-	if *bucket == "" || *key == "" || *accessKey == "" || *accessKey == "" || *pfop == "" {
+	if *bucket == "" || *key == "" || *accessKey == "" || *accessKey == "" || *convert == "" {
 		flag.PrintDefaults()
 		log.Fatalln("invalid args")
 		return
 	}
 	conf.ACCESS_KEY = *accessKey
 	conf.SECRET_KEY = *secretKey
-	fops := *pfop
-	if *saveas != "" {
-		fops = fops + "|saveas/" + base64.URLEncoding.EncodeToString([]byte(*saveas))
+	ops := buildOps(*convert, *saveas)
+	if *file != "" {
+		token := genToken(*bucket, ops, *notify)
+		ret, err := put(token, *key, *file)
+		if err != nil {
+			log.Fatalln("put error", err, ret)
+			return
+		}
+		log.Println("put ret", ret.Key)
+		return
 	}
-
-	client := rs.New(nil)
-	param := url.Values{}
-	param.Set("bucket", *bucket)
-	param.Set("key", *key)
-	param.Set("fops", fops)
-	param.Set("notifyURL", *notify)
-	var ret pfopRet
-	err := client.Conn.CallWithForm(nil, &ret, "http://api.qiniu.com/pfop/", param)
+	ret, err := pfop(ops, *bucket, *key, *notify)
 	if err != nil {
-		log.Fatalln("error", err, ret)
+		log.Fatalln("pfop error", err, ret)
+		return
 	}
-	log.Println("id", ret.PersistentId)
+	log.Println("pfop id", ret.PersistentId)
 }
